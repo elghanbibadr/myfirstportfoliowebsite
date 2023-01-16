a670fa308d5cc07070c2aa965a2b13e76692c0f4
badr ghanbi <bghanbi50@gmail.com>
2023-01-16 16:21:08 +0100

    add typogprahy

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="xl:container text-red-500">
-       <h1 >hello</h1>
+    <div className="xl:container h-screen ">
+       <h1 className='text-transparent text-4xl' >Ashraf Chowdury.
+I Am A Front-End Developer.
+I love to help people use Web technologies such as Java</h1>
       </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,}",
+    "index.html./src/**/*.{js,jsx,}",
   ],
   darkMode:'class',
   theme: {
       container: {
         padding: '2rem',
          center:true
+      },
+      colors: {
+        "white":"#fff",
+        "bodyBg":"#1B2430",
+        'regalBlue': '#243c5a',
       },
     
   },
