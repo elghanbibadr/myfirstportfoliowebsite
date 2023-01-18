44899f80d8cf06ad42410abb375289d3e3ecd78c
badr ghanbi <bghanbi50@gmail.com>
2023-01-18 17:18:27 +0100

    this sheet won't work

--- src/component/main/Skills.jsx
+++ src/component/main/Skills.jsx
@@ -1,45 +1,64 @@
-import React, { useEffect } from 'react'
-import TagCloud from 'TagCloud'
+import React, { useState, useEffect } from 'react';
+import TagCloud from 'TagCloud';
 
-const Skills = () => {
-    useEffect(()=>{
-        return()=>{
-            const container=".tagcloud";
-            const texts=[
-                "HTML",
-                "CSS",
-                "Javascirpt",
-                "GIT",
-                "BOOTSTRAP",
-                "REACT",
-                "TYPESCRIPT",
-                "FIREBASE",
-                "CYPRESS",
-                "NEXT JS",
-                "TAILWIND",
-                "SASS",
-            ];
-            const option={
-                radius:240,
-                maxSpeed:"fast",
-                initSpeed:"normal",
-                keep:true,
-                itemClass:'tagItems',
-                direction:100
-            };
-            TagCloud(container,texts,option)
-        }
-    },[])
+const Skills = () => { 
+  const [option, setOption] = useState({
+    radius: 200,
+    maxSpeed: "fast",
+    initSpeed: "normal",
+    keep: true,
+    itemClass: 'tagItems',
+    direction: 100
+  });
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 700) {
+        setOption({ ...option, radius:400});
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      console.log('effect 1 ruuning')
+    };
+  }, [option]);
+
+  useEffect(() => {
+    return()=>{
+      const container = ".tagcloud";
+      const texts = [
+        "HTML",
+        "CSS",
+        "Javascirpt",
+        "GIT",
+        "BOOTSTRAP",
+        "REACT",
+        "TYPESCRIPT",
+        "FIREBASE",
+        "CYPRESS",
+        "NEXT JS",
+        "TAILWIND",
+        "SASS",
+        "API",
+        "GITHUB",
+        "FRAMER MOTION"
+      ];
+      console.log('runung')
+  
+      TagCloud(container, texts, option);
+
+    }
+  }, [handleResize]);
 
   return (
     <section className='skills flex p-8 flex-col items-center justify-center'>
-        <h2 className='text-white font-bold text-xl md:text-3xl'>Skills</h2>
-        <div className="tagCloudWrraper ">
-            <span className='tagcloud'></span>
-            </div>
+      <h2 className='text-white font-bold text-xl md:text-3xl'>Skills</h2>
+      <div className="tagCloudWrraper">
+        <span className='tagcloud'/> 
+      </div>
     </section>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
