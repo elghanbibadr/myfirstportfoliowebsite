c53f49f1377a872c8687bf06a5ab926f857d2517
badr ghanbi <bghanbi50@gmail.com>
2023-01-19 15:31:18 +0100

    add more projects

--- src/component/main/Data.js
+++ src/component/main/Data.js
@@ -1,5 +1,8 @@
 import multiStepFormDesign from '../../assets/multiStepProjectImage.jpg'
-
+import devJobs from '../../assets/devJobs.png'
+import invoiceapp from '../../assets/invoiceapp.png'
+import shortenUrl from '../../assets/shortenUrl.png'
+import passwordGenerator from '../../assets/passwordGenerator.png'
 export const data=[
     {
      name:"Multi Step Form",
@@ -8,21 +11,27 @@ export const data=[
      technologie:["html","css","React","responsiveDesign","formik"]
     },
     {
-     name:"Multi Step Form",
-     src:multiStepFormDesign,
-     desc:"A multi-step form is a web-based interface that allows users to complete a complex process in a step-by-step manner. These forms typically include several sections, each with its own set of fields and instructions, that guide the user through the process of providing information and making selections.",
-     technologie:["html","css","React","responsiveDesign","formik"]
+     name:"devJobs",
+     src:devJobs,
+     desc:' front-end web application built using React.js that allows users to search for technology jobs using various filters such as job name, location, and job type (full-time or part-time). It also includes a feature where users can view detailed information about a selected job ',
+     technologie:["html","tailwind","responsiveDesign","React"]
     },
     {
-     name:"Multi Step Form",
-     src:multiStepFormDesign,
-     desc:"A multi-step form is a web-based interface that allows users to complete a complex process in a step-by-step manner. These forms typically include several sections, each with its own set of fields and instructions, that guide the user through the process of providing information and making selections.",
-     technologie:["html","css","React","responsiveDesign","formik"]
+     name:"Invoice App",
+     src:invoiceapp,
+     desc:' front-end web application that allows users to manage and track their invoices. Users can add or delete invoices, view the status of their invoices (pending, paid, or draft), and view detailed information about the invoices and their owners. The application is designed to help users keep track of their financial transactions and manage their invoicing process.',
+     technologie:["html","Tailwind","React","responsiveDesign"]
     },
     {
-     name:"Multi Step Form",
-     src:multiStepFormDesign,
-     desc:"A multi-step form is a web-based interface that allows users to complete a complex process in a step-by-step manner. These forms typically include several sections, each with its own set of fields and instructions, that guide the user through the process of providing information and making selections.",
-     technologie:["html","css","React","responsiveDesign","formik"]
+     name:"UrlShorteningApp",
+     src:shortenUrl,
+     desc:'React shortening URL app is a web application that allows users to input a long URL and receive a shortened version of that URL in return. The shortened URL redirects to the original URL when clicked on. The application is built using the React JavaScript library',
+     technologie:["html","Sass","React","axios"]
+    },
+    {
+     name:"Password Generator",
+     src:passwordGenerator,
+     desc:' A JavaScript password generator app would be a program that uses JavaScript code to generate a random password for the user. The app could include options for the user to specify the length and complexity of the password, as well as any specific characters or patterns they would like to include.',
+     technologie:["html","Sass","Javascript"]
     },
 ]
--- src/component/main/Project.jsx
+++ src/component/main/Project.jsx
@@ -4,7 +4,7 @@ import { data } from './Data'
 const Project = () => {
   return (
     <section className='text-center '>
-    <h2 className='text-white text-2xl'>Projects</h2>
+    <h2 className='text-white text-2xl md:text-3xl my-20'>Projects</h2>
     <div className='md:grid grid-cols-2'>
         {data.map(({name,src,technologie,desc},index)=>{
         return <ProjectCard key={index} name={name} src={src} technologie={technologie} desc={desc} />
