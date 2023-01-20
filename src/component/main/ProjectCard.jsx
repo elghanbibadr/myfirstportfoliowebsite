1d8ec6a0316d595488a9cde970bf7d0c0021be96
badr ghanbi <bghanbi50@gmail.com>
2023-01-20 15:12:21 +0100

    add  the arrows to poject links

--- src/component/main/ProjectCard.jsx
+++ src/component/main/ProjectCard.jsx
@@ -4,15 +4,16 @@ const ProjectCard = (props) => {
     <div className='  flex flex-col   mx-auto  bg-secondColor text-white    m-4 md:w-3/4 '>
      <div className='p-4 projectImg-wrraper h-80'><img className='rounded-md h-full  mx-auto' src={props.src} /></div>
      <div className="p-4">
-       <h3 className='text-xl my-6'>{props.name}</h3>
-       <p className='text-sm my-8 md:text-xl'>{props.desc}</p>
+       <h3 className='text-xl font-medium my-6 md:text-2xl'>{props.name}</h3>
+       <p className='text-sm  my-8 md:text-xl text-left'>{props.desc}</p>
        <div>
           <ul className='flex flex-wrap gap-2  justify-between md:flex-nowrap'>{props.technologie.map(tech=>{
               return <li className='bg-regalBlue text-sm   p-2 md:text-xl'>{tech}</li>
             })}</ul>
             <div className="links flex justify-between mt-4">
               <a className='' href={props.href}>View</a>
-              <a className='' href={props.href}>Code</a>
+                <a className='' href={props.href}>Code</a>
+                
               </div>
        </div>
      </div>
