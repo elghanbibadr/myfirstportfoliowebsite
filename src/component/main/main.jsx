6036d94e030741c89e7945db11be103dca800b75
badr ghanbi <bghanbi50@gmail.com>
2023-01-22 11:28:37 +0100

    create a custom bouncing ball

--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Skills from './Skills'
 import Project from './Project'
 import Experience from './Experience'
+import Callout from './Callout'
 const Main = () => {
   return (
     <main>
         <Skills/>
         <Project />
         <Experience/>
+        <Callout/>
     </main>
   )
 }
