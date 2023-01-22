d36db293f708180056a44c6823e1d114ab5ab26d
badr ghanbi <bghanbi50@gmail.com>
2023-01-22 14:22:29 +0100

    add thank you page when submit the form

--- src/component/main/Contact.jsx
+++ src/component/main/Contact.jsx
@@ -31,6 +31,9 @@ const Contact = () => {
 
         />
       <br />
+      <input type="hidden" name="_next" value="thankU.htm" />
+
+      <br />
         <input
           type="email"
           name="email"
