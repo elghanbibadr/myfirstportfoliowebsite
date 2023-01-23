152bca1e308aebf0a44818d1e4151e6a5bcef4ca
badr ghanbi <bghanbi50@gmail.com>
2023-01-23 14:38:41 +0100

    handle form submition errors

--- src/component/main/Contact.jsx
+++ src/component/main/Contact.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { motion } from 'framer-motion';
 
 const Contact = () => {
@@ -24,12 +24,17 @@ const Contact = () => {
       setShowError(true)
       return;
     }else{
-      // alert('truee')
       setShowError(false)
       console.log("subited")
-  //  myForm.submit();
     }
   };
+
+  useEffect(()=>{
+    if (showError){
+      setTimeout(()=>setShowError(false),3000)
+    }
+  },[showError])
+
   return (
     <section id='contact' className='md:w-3/4 mx-auto text-center' >
       <h2 className='text-white mb-10 font-bold text-xl md:text-3xl'>Contact Me</h2>
@@ -80,11 +85,6 @@ const Contact = () => {
       <svg className='h-6 inline-flex' fill='red' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zm32 224c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/></svg>
       <p className='text-secondColor mx-2   inline-flex '> fillup  all the fields.</p>
     </div>}
-  
-    {!showError &&  <div className='fixed flex items-center top-0 left-0 py-2 px-4  rounded-md  w-fit right-0 mx-auto bg-white'>
-      <p className='text-secondColor mx-2   inline-flex '> submited succesfully.</p>
-    </div>
-  }
       
       </motion.div>
     </section>
